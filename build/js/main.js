00d2000fdd22a34f972177d0c65ae3d4988dba52
jishuv1 <jigyashuverma@gmail.com>
2023-05-07 22:21:36 +0545

    ch: Start Here

---
+++ build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let username = 'jishu verma';
+console.log(username);
+let a = 12;
+let b = 4;
+let c = 2;
+console.log(a / b);
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+let username = 'jishu verma';
+console.log(username);
+
+let a: number = 12;
+let b: number = 4;
+let c: number = 2;
+
+console.log(a / b);
