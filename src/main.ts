27ead14e0e0b9d2203cc46c2d797fe82672dcf8f
jishuv1 <jigyashuverma@gmail.com>
2023-05-08 13:19:40 +0545

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-let username = 'jishu verma';
-console.log(username);
-
-let a: number = 12;
-let b: number = 4;
-let c: number = 2;
-
-console.log(a / b);
+type Guitarist = {
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+};
