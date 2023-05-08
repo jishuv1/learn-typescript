b49e0d4a14b7d009ab7517feb282c990999badd9
jishuv1 <jigyashuverma@gmail.com>
2023-05-08 20:24:01 +0545

    ch-7 Index Signatures and Key of Assertions

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,44 @@
 "use strict";
-let username = 'jishu verma';
-console.log(username);
-let a = 12;
-let b = 4;
-let c = 2;
-console.log(a / b);
+// Index Signatures
+const todaysTransactions = {
+    pizza: -10,
+    book: -5,
+    job: 50,
+};
+console.log(todaysTransactions.pizza);
+console.log(todaysTransactions['pizza']);
+let prop = 'pizza';
+console.log(todaysTransactions[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+// todaysTransactions.pizza = 40;
+console.log(todaysNet(todaysTransactions));
+const student = {
+    name: 'Doug',
+    GPA: 3.5,
+    classes: [100, 200],
+};
+// console.log(student.test);
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+Object.keys(student).map((key) => {
+    console.log(`${key}: ${student[key]}`);
+});
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, 'name');
+const monthlyIncomes = {
+    salary: 500,
+    bonuses: 100,
+    sidehustle: 250,
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,87 @@
-let username = 'jishu verma';
-console.log(username);
+// Index Signatures
 
-let a: number = 12;
-let b: number = 4;
-let c: number = 2;
+// interface TransactionObj {
+//   readonly [index: string]: number;
+// }
 
-console.log(a / b);
+interface TransactionObj {
+  readonly [index: string]: number;
+  pizza: number;
+  book: number;
+  job: number;
+}
+
+const todaysTransactions: TransactionObj = {
+  pizza: -10,
+  book: -5,
+  job: 50,
+};
+
+console.log(todaysTransactions.pizza);
+console.log(todaysTransactions['pizza']);
+
+let prop: string = 'pizza';
+console.log(todaysTransactions[prop]);
+
+const todaysNet = (transactions: TransactionObj): number => {
+  let total = 0;
+  for (const transaction in transactions) {
+    total += transactions[transaction];
+  }
+  return total;
+};
+
+// todaysTransactions.pizza = 40;
+
+console.log(todaysNet(todaysTransactions));
+
+////////////////////////////////////////////////////
+
+interface Student {
+  //   [key: string]: string | number | number[] | undefined;
+  name: string;
+  GPA: number;
+  classes?: number[];
+}
+
+const student: Student = {
+  name: 'Doug',
+  GPA: 3.5,
+  classes: [100, 200],
+};
+
+// console.log(student.test);
+
+for (const key in student) {
+  console.log(`${key}: ${student[key as keyof Student]}`);
+}
+
+Object.keys(student).map((key) => {
+  console.log(`${key}: ${student[key as keyof typeof student]}`);
+});
+
+const logStudentKey = (student: Student, key: keyof Student): void => {
+  console.log(`Student ${key}: ${student[key]}`);
+};
+
+logStudentKey(student, 'name');
+
+//////////////////////////////////////////////////////////////////////////////
+
+// interface Incomes {
+//   [key: string]: number;
+// }
+
+type Streams = 'salary' | 'bonuses' | 'sidehustle';
+
+type Incomes = Record<Streams, number | string>;
+
+const monthlyIncomes: Incomes = {
+  salary: 500,
+  bonuses: 100,
+  sidehustle: 250,
+};
+
+for (const revenue in monthlyIncomes) {
+  console.log(monthlyIncomes[revenue as keyof Incomes]);
+}
